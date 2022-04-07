7a677cb9aea96fcda9aaa3f2f220d1a2f564dccc
chaeeunj <official2930@naver.com>
2022-04-08 02:25:55 +0900

    2439번 풀이

--- jungchaeeun/1주차/백준_2439번 문제.py
+++ jungchaeeun/1주차/백준_2439번 문제.py
@@ -1,7 +1,4 @@
 n = int(input())
 
-for i in range(n):
-  for j in range(n):
-    if j >= i:
-      print('*', end='')
-  print('')
+for i in range(1, n+1):
+    print(' '*(n-i) + '*'*i)
--- jungchaeeun/1주차/백준_2440번 문제.py
+++ jungchaeeun/1주차/백준_2440번 문제.py
@@ -2,6 +2,6 @@ n = int(input())
 
 for i in range(n):
   for j in range(n):
-    if j <= i:
+    if j >= i:
       print('*', end='')
   print('')
