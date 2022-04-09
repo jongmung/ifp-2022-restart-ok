69933458fcb471483e5561c2ebf5a6405dec6532
chaeeunj <official2930@naver.com>
2022-04-10 05:12:55 +0900

    5635번 풀이

--- jungchaeeun/1주차/백준_11098번 문제.py
+++ jungchaeeun/1주차/백준_11098번 문제.py
@@ -1,3 +1,5 @@
+'''다른 풀이 연습
+
 n = int(input())
 
 a = []
@@ -10,13 +12,18 @@ for i in range(n):
         a.append(line)
 
 sorted(a, key=lambda a: a[0])
-print(a[0])
+print(a[0])'''
 
-# a = []
+n = int(input())
 
-# line = []
+for _ in range(n):
+    p = int(input())
+    max_price = 0
+    max_name = ""
+    for _ in range(p):
+        c, name = input().split()
+        if int(c) > max_price:
+            max_price = int(c)
+            max_name = name
 
-# line = input().split()
-# a.append(line)
-
-# print(a)
+    print(max_name)
