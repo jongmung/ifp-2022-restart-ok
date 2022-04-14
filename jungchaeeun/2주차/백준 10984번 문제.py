7efd82fdf87efa332dde9f519a43881ea4e13691
chaeeunj <official2930@naver.com>
2022-04-14 22:13:36 +0900

    10984번 풀이

--- jungchaeeun/2주차/백준 10984번 문제.py
+++ jungchaeeun/2주차/백준 10984번 문제.py
@@ -1,8 +1,12 @@
 t = int(input())
+hSum = 0
+gpa = 0
 
 for i in range(t):
     n = int(input())
     for i in range(n):
-        c, g = input().split()
-        c = int(c)
-        g = float(g)
+        c, g = map(str, input().split())
+        hSum += int(c)
+        gpa += float(c)*float(g)
+
+    print(hSum, round(gpa/hSum, 1))
