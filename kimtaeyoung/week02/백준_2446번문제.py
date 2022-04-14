30bf96b223ee5ecc8968f374f68d1d6a7a64da65
overtae <rlaxodud911@gmail.com>
2022-04-14 19:04:26 +0900

    백준 2446번 문제 풀이 수정

--- kimtaeyoung/week02/백준_2446번문제.py
+++ kimtaeyoung/week02/백준_2446번문제.py
@@ -27,6 +27,6 @@
 n = int(input())
 
 for i in range(n, 0, -1):
-    print('{:^{}}'.format('*' * (2 * i - 1), 2 * n))
+    print(' ' * (n - i) + '*' * (2 * i - 1))
 for i in range(2, n+1):
-    print('{:^{}}'.format('*' * (2 * i - 1), 2 * n))
+    print(' ' * (n - i) + '*' * (2 * i - 1))
