12b2f24b871d943def9e4882d2345a3d924d1ce7
chaeeunj <official2930@naver.com>
2022-04-15 22:38:38 +0900

    9325번 문제

--- jungchaeeun/2주차/백준 10833번 문제.py
+++ jungchaeeun/2주차/백준 10833번 문제.py
@@ -6,3 +6,20 @@ for i in range(n):
     sum += apple % student
 
 print(sum)
+
+
+'''
+다른 풀이(우대조건에서 감점-34점 나옴)
+
+n = int(input())
+sum = 0
+
+for i in range(n):
+    student, apple = map(int, input().split())
+    if student >= apple:
+        sum += apple
+    else:
+        sum += apple % student
+
+print(sum)
+'''
