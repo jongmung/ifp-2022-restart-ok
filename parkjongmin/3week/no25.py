1bf37633edc53439db22e92ebf51bb5d4c6eb53d
jongmung <jmp99@naver.com>
2022-04-29 23:10:25 +0900

    no25~26

---
+++ parkjongmin/3week/no25.py
@@ -0,0 +1,7 @@
+#2010_플러그
+import sys
+n = int(input())
+s = 0
+for _ in range(n):
+    s += int(sys.stdin.readline())
+print(s-n+1)
---
+++ parkjongmin/3week/no26.py
@@ -0,0 +1,6 @@
+#5522_카드게임
+s = 0
+for _ in range(5):
+    n=int(input())
+    s += n
+print(s)
