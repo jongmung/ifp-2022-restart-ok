416173225fad0ad1d024ea570dae8c619ecdabc0
TGoddessana <ingstor1092@gmail.com>
2022-05-03 22:00:05 +0900

    정재균:백준2455풀이

--- jungjeagyun/week04/bj_problem_2455.py
+++ jungjeagyun/week04/bj_problem_2455.py
@@ -4,8 +4,10 @@ current_ppl = 0 # 열차에 있던 순간
 current_ppl_list = [0, 0, 0, 0] # 열차에 있던 순간
 
 for i in range(4):
-    fp[i], tp[i] = map(int,input().split())
-    current_ppl = current_ppl - fp[i] + tp[i]
-    current_ppl_list[i] = current_ppl
+    fp[i], tp[i] = map(int,input().split()) # 저장
+    current_ppl = current_ppl - fp[i] + tp[i] # 현재 인간들을 저장
+    current_ppl_list[i] = current_ppl # 현재 순간의 인간 수를 리스트에 저장
+
+
 
 print(max(current_ppl_list))
