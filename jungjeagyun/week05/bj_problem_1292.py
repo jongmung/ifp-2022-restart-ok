13a45cfe5659a1b4cd42055540552aecf9281ecd
TGoddessana <ingstor1092@gmail.com>
2022-05-08 21:22:50 +0900

    정재균:백준1292풀이

--- jungjeagyun/week05/bj_problem_1292.py
+++ jungjeagyun/week05/bj_problem_1292.py
@@ -1,1 +1,20 @@
-x,y = map(int, inpt().split())
+x,y = map(int, input().split())
+
+# ex : 3, 4
+# 1 22 333 4444 55555
+# 2+3 = 5
+
+a,b=map(int,input().split())
+data=[0]
+sum=0
+
+for i in range(1,b+1):
+  for j in range(i):
+    data.append(i)
+
+for i in range(a, b+1):
+  sum+=data[i]
+print(sum)
+
+# 구글의 도움을 받았읍니다.
+# ㅠㅠ
