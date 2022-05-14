7e97a5d6079b2f77050edff8644b3ed4810b7af6
jongmung <jmp99@naver.com>
2022-05-15 00:05:00 +0900

    no62~63

--- parkjongmin/6week/no62.py
+++ parkjongmin/6week/no62.py
@@ -1,1 +1,10 @@
 #9076_점수 집계
+x = int(input())
+for i in range(x):
+    a = list(map(int, input().split()))
+    a.remove(max(a))
+    a.remove(min(a))
+    if max(a) - min(a) >= 4:
+        print('KIN')
+    else:
+        print(sum(a))
--- parkjongmin/6week/no63.py
+++ parkjongmin/6week/no63.py
@@ -1,1 +1,6 @@
 #2693_N번째 큰 수
+a=[]
+for _ in range(int(input())):
+    a = list(map(int,input().split()))
+    b = sorted(a)
+    print(b[7])
