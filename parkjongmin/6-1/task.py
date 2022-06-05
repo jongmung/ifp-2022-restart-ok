bf413831010c85568e2934e1fbf73649421acb37
jongmung <jmp99@naver.com>
2022-06-05 23:10:48 +0900

    task

--- parkjongmin/6-1/task.py
+++ parkjongmin/6-1/task.py
@@ -4,3 +4,20 @@ print("Hello World!")
 #1000_A+B
 a,b=map(int, input().split())
 print(a+b)
+
+#10718_We love kriii
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+
+#1008_A/B
+A,B = map(int,input().split())
+print(A/B)
+
+#1330_두 수 비교
+A,B = map(int,input().split())
+if A > B:
+    print(">")
+elif A < B:
+    print("<")
+else:
+    print("==")
