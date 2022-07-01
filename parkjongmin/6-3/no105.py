cc2018f9b1211c3ff92ffc1fd75073eead869486
jongmung <jmp99@naver.com>
2022-07-01 23:40:27 +0900

    no105~106

---
+++ parkjongmin/6-3/no105.py
@@ -0,0 +1,5 @@
+#11050_이항 계수 1
+from math import factorial
+n, k = map(int, input().split())
+b = factorial(n) // (factorial(k) * factorial(n - k))
+print(b)
---
+++ parkjongmin/6-3/no106.py
@@ -0,0 +1,6 @@
+#15552_빠른 A+B
+import sys
+h = int(input())
+for i in range(h):
+        a,b = map(int, sys.stdin.readline().split())
+        print(a+b)
