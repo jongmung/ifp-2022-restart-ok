71cfbb3485a4274e907b7befacc715f67786da39
tjqehd23 <tjqehd23@naver.com>
2022-08-01 14:43:53 +0900

    ahndongsup : June ~ July

--- AhnDongSup/6_June/week_02/ex220606_핸드폰 번호 가리기.py
+++ AhnDongSup/6_June/week_02/ex220606_핸드폰 번호 가리기.py
@@ -5,7 +5,6 @@
 #     print(*ans)
 # solution(list(input()))
 
-
 # def solution(ans):
 #     for i in range(len(ans)-4):
 #         ans=ans.replace(ans[i],'*',1)
@@ -19,4 +18,4 @@ print(solution('01075675023'))
 # def solution(ans):
 #     return "*"*len(ans[:-4])+ans[-4:]
 # print(solution('01075675023'))
-# programmers 12948
+
