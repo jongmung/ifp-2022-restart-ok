28c7e7550edef7a7c47c8f10006cfec821bd3240
taeyoung <rlaxodud911@gmail.com>
2022-08-26 03:27:38 +0900

    프로그래머스 12910번 문제 풀이

---
+++ kimtaeyoung/8월/3주차/programmers_no12910.py
@@ -0,0 +1,23 @@
+# 나누어 떨어지는 숫자 배열
+
+''' 문제 설명
+array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+'''
+
+''' 제한 조건
+- arr은 자연수를 담은 배열입니다.
+- 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+- divisor는 자연수입니다.
+- array는 길이 1 이상인 배열입니다.
+'''
+
+# 풀이
+
+
+def solution(arr, divisor):
+    answer = [x for x in sorted(arr) if x % divisor == 0]
+    return answer if answer else [-1]
+
+    # 또는,
+    return [x for x in sorted(arr) if x % divisor == 0] or [-1]
