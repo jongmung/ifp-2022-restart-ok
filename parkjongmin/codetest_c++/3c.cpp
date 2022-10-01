d9a1bc76dfd6b8dd93a5bd32e013e1282ddb29f1
jongmung <jmp99@naver.com>
2022-10-01 23:50:23 +0900

    c++3codetest

---
+++ parkjongmin/codetest_c++/3c.cpp
@@ -0,0 +1,28 @@
+//첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
+#include <iostream>
+using namespace std;
+int main(){
+    string a;
+    cin >> a;
+    cout << a << "??!" << endl;
+    return 0;}
+
+//불기 연도를 서기 연도로 변환한 결과를 출력한다.
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    cout << a-543 << endl;
+    return 0;
+}
+
+//첫째 줄에 동혁이가 찾은 흰색 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수가 주어진다.
+//이 값은 0보다 크거나 같고 10보다 작거나 같은 정수이다.
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+    
+}
