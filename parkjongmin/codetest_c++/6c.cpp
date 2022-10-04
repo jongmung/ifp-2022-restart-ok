117e22882c4a78bbe49e563796989ec9a5395533
jongmung <jmp99@naver.com>
2022-10-05 01:40:36 +0900

    c++6codetest

--- parkjongmin/codetest_c++/6c.cpp
+++ parkjongmin/codetest_c++/6c.cpp
@@ -36,3 +36,24 @@ int main(){
 //2439_별 찍기-2
 //첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 //하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        for (int k=0;k<n-i;k++){ // 공백은 n-i개 만큼 출력
+            cout<<' ';
+        }
+        for (int k=1;k<=i;k++){  // 별은 i개만큼 출력
+            cout<<"*";
+        }
+        cout<<'\n';
+    }
+    return 0;
+}
+
+//10871_x보다 작은수
+//정수 N개로 이루어진 수열 A와 정수 X가 주어진다.
+//이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
