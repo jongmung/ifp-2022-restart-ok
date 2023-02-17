86bd369890799434f718fa6aad44546c673feb5f
jongmung <jmp99@naver.com>
2023-02-17 23:39:47 +0900

    coding

--- parkjongmin/codingtest/1week.py
+++ parkjongmin/codingtest/1week.py
@@ -19,3 +19,27 @@ def solution(num1, num2):
     answer = num1//num2
     return answer
 
+# 숫자 비교하기
+# 정수 num1과 num2가 매개변수로 주어집니다.
+# 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = 0
+    if num1 == num2:
+        answer = 1
+    else:
+        answer = -1
+    return answer
+
+# 나머지 구하기
+# 정수 num1, num2가 매개변수로 주어질 때,
+# num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+# 나이 출력
+# 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+# 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+def solution(age):
+    answer = 2023 - int(age)
+    return answer
