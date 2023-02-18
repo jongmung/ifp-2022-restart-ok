df6465201054262f5ec8dfd15c08e382e5b69c6e
jongmung <jmp99@naver.com>
2023-02-18 18:22:31 +0900

    codingst

--- parkjongmin/codingtest/1week.py
+++ parkjongmin/codingtest/1week.py
@@ -43,3 +43,40 @@ def solution(num1, num2):
 def solution(age):
     answer = 2023 - int(age)
     return answer
+
+# 두 수의 합
+# 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
+
+# 각도기
+# 각에서 0도 초과 90도 미만은 예각,
+# 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
+# 각 angle이 매개변수로 주어질 때
+# 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+def solution(angle):
+    answer = 0
+    if 0 < angle < 90:
+        answer = 1
+    elif angle == 90:
+        answer = 2
+    elif 90 < angle < 180:
+        answer = 3
+    elif angle == 180:
+        answer = 4
+    return answer
+# 다른 사람 풀이
+def solution(angle):
+    answer = (angle // 90) * 2 + (angle % 90 > 0) * 1 # t of f로 계산되어 1 or 0
+    return answer
+
+# 두 수의 나눗셈
+# 정수 num1과 num2가 매개변수로 주어질 때,
+# num1을 num2로 나눈 값에
+# 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = (num1/num2) * 1000
+    return int(answer)
+
+
