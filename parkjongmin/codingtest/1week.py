5a371183486bca8098a15dbc4dbc3094cd53e139
jongmung <jmp99@naver.com>
2023-02-18 18:34:14 +0900

    codingst

--- parkjongmin/codingtest/1week.py
+++ parkjongmin/codingtest/1week.py
@@ -79,4 +79,13 @@ def solution(num1, num2):
     answer = (num1/num2) * 1000
     return int(answer)
 
+# 배열의 평균값
+# 정수 배열 numbers가 매개변수로 주어집니다.
+# numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+def solution(numbers):
+    answer = sum(numbers)/len(numbers)
+    return answer
+
+
+
 
